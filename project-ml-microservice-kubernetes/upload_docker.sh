44bbcfef8758f8a992d71278bfd3a3d8302154b5
chibkay <chibk.okoli@gmail.com>
2022-07-17 23:41:29 +0100

    final version

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,6 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=chibkay/dev
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,5 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker tag dev:latest chibkay/dockerhub:dev
+docker push chibkay/dockerhub:dev
